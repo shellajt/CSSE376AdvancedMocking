c5b8ea0f157df8d11f250282da3f5e09718d597b
Jesse Shellabarger <shellajt@rose-hulman.edu>
2015-04-21 23:07:25 -0400

    Tasks 2 & 3 complete

--- CommandClientVisualStudioTest/AdvancedMockTests.cs
+++ CommandClientVisualStudioTest/AdvancedMockTests.cs
@@ -66,13 +66,60 @@ namespace CommandClientVisualStudioTest
         [TestMethod]
         public void TestUserExitCommandWithoutMocks()
         {
-            Assert.Fail("Not yet implemented");
+            IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
+            Command command = new Command(CommandType.UserExit, ipaddress, null);
+            MemoryStream mockStream = new MemoryStream();
+            byte[] commandBytes = { 0, 0, 0, 0 };
+            byte[] ipLength = { 9, 0, 0, 0 };
+            byte[] ip = { 49, 50, 55, 46, 48, 46, 48, 46, 49 };
+            byte[] metaDataLength = { 2, 0, 0, 0 };
+            byte[] metaData = { 10, 0 };
+
+            String commandBytesString = "0000";
+            String ipLengthString = "9000";
+            String ipString = "495055464846484649";
+            String metaDataLengthString = "2000";
+            String metaDataString = "100";
+
+            CMDClient client = new CMDClient(null, "Bogus network name");
+            typeof(CMDClient).GetField("networkStream", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(client, mockStream);
+            client.SendCommandToServerUnthreaded(command);
+            byte[] data = mockStream.ToArray();
+            String dataString = "";
+            for (int i = 0; i < data.Length; i++)
+            {
+                dataString += data[i].ToString();
+            }
+
+            Assert.IsTrue(dataString.Contains(commandBytesString));
+            Assert.IsTrue(dataString.Contains(ipLengthString));
+            Assert.IsTrue(dataString.Contains(ipString));
+            Assert.IsTrue(dataString.Contains(metaDataLengthString));
+            Assert.IsTrue(dataString.Contains(metaDataString));
+
         }
 
         [TestMethod]
         public void TestSemaphoreReleaseOnNormalOperation()
         {
-            Assert.Fail("Not yet implemented");
+            System.IO.Stream mockStream = mocks.DynamicMock<System.IO.Stream>();
+            System.Threading.Semaphore mockSemaphore =  mocks.DynamicMock<System.Threading.Semaphore>();
+
+            using (mocks.Ordered())
+            {
+                Expect.Call(mockSemaphore.WaitOne()).Return(true);
+                Expect.Call(mockSemaphore.Release()).Return(1);
+            }
+            mocks.ReplayAll();
+            CMDClient client = new CMDClient(null, "Bogus network name");
+            typeof(CMDClient).GetField("networkStream", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(client, mockStream);
+            typeof(CMDClient).GetField("semaphore", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(client, mockSemaphore);
+
+
+
+
+            client.SendCommandToServerUnthreaded(new Command(CommandType.UserExit, IPAddress.Parse("127.0.0.1"), null));
+            mocks.VerifyAll();
         }
 
         [TestMethod]
